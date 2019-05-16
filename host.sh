58de2c3ac0258b8c7c78b694df83c6498ad03d7f
TELIPRAYUJA <prayujawork@gmail.com>
2019-05-16 17:18:47 +0530

    Shows Some IP related data

---
+++ host.sh
@@ -0,0 +1,16 @@
+#Display the domain name
+hostname -d
+#Display IP address
+hostname -i
+#Display current weeek number
+date +%u
+#Display linux Flav name
+uname -s
+#Display name of processor
+uname -p
+#Display DayNum in year
+cat -j
+#Display date in yyyy/mm/dd
+date +%F
+echo ALL COMMANDS SUCCSSFULLY COMPILED
+
