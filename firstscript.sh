07fb4b59726cd05e0d17726a0b154d317c804377
TELIPRAYUJA <prayujawork@gmail.com>
2019-05-16 16:59:23 +0530

    This file contains users information of local system

---
+++ firstscript.sh
@@ -0,0 +1,7 @@
+#number of Users
+
+echo Currently logged in users are `who | wc -l`
+echo Currently logged in users are `who -q`
+echo Logged in users count is: `users | wc -w`
+echo "Program success"
+
