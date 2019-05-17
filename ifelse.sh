38d8afd8617fb0f9d28fbb6bfdf9404455ae87d3
TELIPRAYUJA <prayujawork@gmail.com>
2019-05-17 17:12:50 +0530

    This file contain if-else condition program

---
+++ ifelse.sh
@@ -0,0 +1,7 @@
+# Syntax of simple if then statement
+if [ 35 -gt 0 ]
+#if [ 35 -gt 45] in this case it will print lesses after passing through eacho and then command
+ 
+then
+    echo "Greater"
+fi
